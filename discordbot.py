3901268cd56f02efed0845cc5d44d2ac648fe4e5
Meshikona <50591103+Meshikona@users.noreply.github.com>
2019-09-03 04:20:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,6 +10,25 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
+
 
 @bot.command()
 async def ping(ctx):
